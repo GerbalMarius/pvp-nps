da3e13b926b9c5c53141898e9f72d5c99aba9a71
Dominykas Burba <140396442+MrDomke@users.noreply.github.com>
2025-03-05 21:17:23 +0200

    Login page form functionality
    * Created pages for Login Page
    
    * Added login page
    
    * Added alerts for validation errors
    
    ---------
    
    Co-authored-by: MrDomke <iksiukas25@gmail.com>

---
+++ nps/nps/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using nps.Models;
+
+namespace nps.Pages.Login
+{
+    public class IndexModel : PageModel
+    {
+		[BindProperty]
+
+        public required UserInfo UserInfo { get; set; }
+        public IActionResult OnPost()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+			return RedirectToPage("/Index");
+		}
+    }
+}
