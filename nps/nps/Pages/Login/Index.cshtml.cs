9fed455649721fcd785f3dca72038eaa62279365
MrDomke <iksiukas25@gmail.com>
2025-03-05 18:26:46 +0200

    Created pages for Login Page

---
+++ nps/nps/Pages/Login/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace nps.Pages.Login
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
