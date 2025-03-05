2ad1f1765bb50b649ffc1680669bdc020239d699
Marius <mariukas.ambrazevicius@gmail.com>
2025-03-05 11:21:46 +0200

    General comments and notices about submitted code.

--- nps/nps/Models/UserInfo.cs
+++ nps/nps/Models/UserInfo.cs
@@ -1,5 +1,8 @@
 ﻿namespace nps.Models
 {
+    //Better variant would be to use immutable dataclass for transfer , because dtos don't model any real world data,
+    //Making them mutable could cause undefined behavior inbetween transfers if more than one user performs the same action.
+    //Easiest way to do this is to create records or classes with get; init accessors.
     public class UserInfo
     {
         public string Email { get; set; }
--- nps/nps/Pages/UserFunctions/UserAddition.cshtml.cs
+++ nps/nps/Pages/UserFunctions/UserAddition.cshtml.cs
@@ -2,20 +2,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using nps.Models;
 
-namespace nps.Pages.AddUser
+namespace nps.Pages.UserFunctions
 {
     public class UserAdditionModel : PageModel
     {
         [BindProperty]
         public UserInfo UserInfo { get; set; }
 
-        public List<UserInfo> UserInfoList = new List<UserInfo>();    
+        //Better to keep immutable for outside access
+        //public List<UserInfo> UserInfoList { get; init; } = [];
+        // since .Net 8.0 all collections that implement IEnumerable<T>, except for IDictionary<TKey, TValue>,
+        // can be initialised using "collection expression" https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/collection-expressions
+        public List<UserInfo> UserInfoList = new List<UserInfo>();  
+        
         public void OnGet()
         {
         }
 
         public IActionResult OnPost()
         {
+            //for this to work the data transfer model should've been anotaded with annotations defined in System.Component.Model.DataAnnotations library
+            //for example [Required], [MaxLength] etc.
             if(!ModelState.IsValid)
             {
                 return Page();
