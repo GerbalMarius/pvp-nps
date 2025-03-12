47c2fd7068867cba39852c710ae8e81096c326bc
Marius <mariukas.ambrazevicius@gmail.com>
2025-03-12 16:20:17 +0200

    Cleanup.

--- nps/nps/Pages/UserFunctions/UserAddition.cshtml.cs
+++ nps/nps/Pages/UserFunctions/UserAddition.cshtml.cs
@@ -8,11 +8,7 @@ namespace nps.Pages.UserFunctions
     {
         [BindProperty]
         public UserInfo UserInfo { get; set; }
-
-        //Better to keep immutable for outside access
-        //public List<UserInfo> UserInfoList { get; init; } = [];
-        // since .Net 8.0 all collections that implement IEnumerable<T>, except for IDictionary<TKey, TValue>,
-        // can be initialised using "collection expression" https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/collection-expressions
+        
         public List<UserInfo> UserInfoList = new List<UserInfo>();  
         
         public void OnGet()
@@ -21,8 +17,6 @@ namespace nps.Pages.UserFunctions
 
         public IActionResult OnPost()
         {
-            //for this to work the data transfer model should've been anotaded with annotations defined in System.Component.Model.DataAnnotations library
-            //for example [Required], [MaxLength] etc.
             if(!ModelState.IsValid)
             {
                 return Page();
