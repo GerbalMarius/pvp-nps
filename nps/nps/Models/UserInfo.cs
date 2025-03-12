4458d909d96929942ede22491107643a342dfe64
Marius <mariukas.ambrazevicius@gmail.com>
2025-03-12 15:16:35 +0200

    removed unneeded comments.

--- nps/nps/Models/UserInfo.cs
+++ nps/nps/Models/UserInfo.cs
@@ -2,9 +2,6 @@
 
 namespace nps.Models
 {
-    //Better variant would be to use immutable dataclass for transfer , because dtos don't model any real world data,
-    //Making them mutable could cause undefined behavior inbetween transfers if more than one user performs the same action.
-    //Easiest way to do this is to create records or classes with get; init accessors.
     public class UserInfo
     {
 		[Required(ErrorMessage = "Email is required")]
