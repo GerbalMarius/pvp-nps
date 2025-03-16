69e45c7d5b0fae51f3675ecf9fd92dd16cf5777b
Marius <mariukas.ambrazevicius@gmail.com>
2025-03-16 20:58:00 +0200

    email validation fix.

--- nps/nps/Models/LoginInfo.cs
+++ nps/nps/Models/LoginInfo.cs
@@ -5,7 +5,7 @@ namespace nps.Models
     public record LoginInfo
     {
 		[Required(ErrorMessage = "Email is required")]
-		[RegularExpression( "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(.[A-Za-z0-9]+)*([A-Za-z]{2,})$", 
+		[RegularExpression( @"^(?=.{1,64}@)[\p{L}0-9_-]+(\.[\p{L}0-9_-]+)*@[^-][\p{L}0-9-]+(\.[\p{L}0-9-]+)*(\.[\p{L}]{2,})$", 
 							ErrorMessage = "Please enter a valid email address")]
 		public string Email { get; init; } = string.Empty;
 
