f12858d0ec63c750d6480527544587c89471e409
MrDomke <iksiukas25@gmail.com>
2025-03-17 22:12:38 +0200

    fixed

--- nps/nps/Pages/Login/Index.cshtml.cs
+++ nps/nps/Pages/Login/Index.cshtml.cs
@@ -13,53 +13,20 @@ namespace nps.Pages.Login
 {
 	public class IndexModel : PageModel
 	{
-		private readonly IHttpContextAccessor _httpContextAccessor;
-		public IndexModel(IHttpContextAccessor httpContextAccessor)
-		{
-			_httpContextAccessor = httpContextAccessor;
-		}
 
 		[BindProperty]
 
 		public required LoginInfo LoginInfo { get; set; }
-		public async Task<IActionResult> OnPostAsync()
+		public IActionResult OnPost()
 		{
 			if (!ModelState.IsValid)
 			{
 				return Page();
 			}
 
-			// Note : Sutvarkyti pilnai kai bus db
-			if (LoginInfo.Email == "admin@gmail.com" && LoginInfo.Password == "password123!")
-			{
-				var userId = "12345";
-
-				var claims = new List<Claim>
-				{
-					new Claim(ClaimTypes.Name, LoginInfo.Email),
-					new Claim("UserId", userId)
-				};
-
-				var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-				var authProperties = new AuthenticationProperties
-				{
-					IsPersistent = true,
-					ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(30)
-				};
-
-				await HttpContext.SignInAsync(
-					CookieAuthenticationDefaults.AuthenticationScheme,
-					new ClaimsPrincipal(claimsIdentity),
-					authProperties
-				);
-
-				_httpContextAccessor.HttpContext?.Session.SetString("UserId", userId);
+			
 
 
-				return RedirectToPage("/Index");
-			}
-
-			ModelState.AddModelError(string.Empty, "Invalid email or password");
 			return Page();
 		}
 	}
