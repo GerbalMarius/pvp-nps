5719bd453b233c7a6ca5f7fdaf180dc6f4aaf701
Marius <mariukas.ambrazevicius@gmail.com>
2025-03-21 17:07:10 +0200

    Few insignificant fixes.

--- nps/nps/Migrations/Data/AppDbContext.cs
+++ nps/nps/Migrations/Data/AppDbContext.cs
@@ -6,9 +6,7 @@ namespace nps.Migrations.Data;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<Survey> Surveys { get; set; }  
-    public DbSet<Question> Questions { get; set; }
-    public DbSet<Response> Responses { get; set; }
+   
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
