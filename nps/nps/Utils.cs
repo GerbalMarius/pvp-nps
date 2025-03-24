cdb4e14e7638a3119a7c88b833873df27839838c
Marius Ambrazevičius <160462926+GerbalMarius@users.noreply.github.com>
2025-03-24 18:20:19 +0200

    Few generalistic fixes and added a STATIC utility method to calculate nps score based on the provided responses.
    * Added a query based method to calc basic nps score.
    
    * Removed AnswerType enum in favor of table per type hierarchies.

---
+++ nps/nps/Utils.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nps.Models;
+
+namespace nps;
+
+public static class Utils
+{
+    /// <summary>
+    /// Calculates nps score based on the formula = %promoters - %detractors,
+    /// if nps score needs to be extracted within a given timeframe, filter the corresponding surveys by Datetime property 'TakenAt'.
+    /// </summary>
+    /// <param name="responses">The table of corresponding question responses.</param>
+    /// <returns>The calculated nps score wrapped in a non-blocking task.</returns>
+    public static async Task<double> CalcNpsScore(IQueryable<Response> responses)
+    {
+        var groupedResponses = await responses
+            .Where(response => response.NumberRating.HasValue)
+            .GroupBy(response => response.NumberRating >= 9 ? "Promoters" : response.NumberRating <= 6 ? "Detractors" : "Passives")
+            .Select(group => new
+            {
+                Category = group.Key, 
+                Count = group.Count()
+            }) //Selects are non-tracking by default
+            .ToListAsync();//to allow non-consuming behavior
+
+        
+        int totalResponses = groupedResponses.Sum(group => group.Count);
+        if (totalResponses == 0) return 0;
+
+        
+        int promoters = groupedResponses.FirstOrDefault(group => group.Category == "Promoters")?.Count ?? 0;
+        int detractors = groupedResponses.FirstOrDefault(group => group.Category == "Detractors")?.Count ?? 0;
+
+        // Calculate percentages & NPS score
+        double promoterPercentage = (double)promoters / totalResponses * 100;
+        double detractorPercentage = (double)detractors / totalResponses * 100;
+
+        return Math.Round(promoterPercentage - detractorPercentage);
+    }
+}
