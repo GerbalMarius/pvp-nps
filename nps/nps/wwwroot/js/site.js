f8def6cf1e7f395aee11398797d6dc2c5740a744
Marius Ambrazevičius <160462926+GerbalMarius@users.noreply.github.com>
2025-04-03 16:15:59 +0300

    Additional ui styles. (#18)
    * loading spinner for improved ux (might remove later, just testing.)
    
    * styling for vertical admin nav-bar, dropdown window and a bunch of scripts for their animation transitions.

--- nps/nps/wwwroot/js/site.js
+++ nps/nps/wwwroot/js/site.js
@@ -1,20 +1,54 @@
 ﻿function selectNPS(value, name, questionId) {
-    // Get all buttons in the same question container
+
     let questionContainer = document.getElementById(`question-${questionId}`);
     let buttons = questionContainer.querySelectorAll('.rating-btn');
 
-    // Remove active class from all buttons in this question only
     buttons.forEach(btn => btn.classList.remove('active'));
 
-    // Add active class to the selected button
+
+    
+    let questionContainer = document.getElementById(`question-${questionId}`);
+    let buttons = questionContainer.querySelectorAll('.rating-btn');
+
+    
+    buttons.forEach(btn => btn.classList.remove('active'));
+
+
     let selectedButton = questionContainer.querySelector(`.rating-btn[data-value="${value}"]`);
     if (selectedButton) {
         selectedButton.classList.add('active');
     }
 
-    // Update hidden input field
     let inputField = document.getElementById(name);
     if (inputField) {
         inputField.value = value;
     }
+
 }
+
+//used for expansion of navbar through clicking of burger menu.
+function toggleNavBar(event, navBarId) {
+    event.stopPropagation();
+    document.getElementById(navBarId).classList.toggle('expanded');
+}
+//if clicked outside of menu, shrink it to show only icons.
+function shrinkNavBar(event, navBarId) {
+    let navBar = document.getElementById(navBarId);
+    if (navBar.classList.contains('expanded')) {
+        navBar.classList.remove('expanded');
+    }
+}
+//opening of dropdown
+function toggleDropDown() {
+    let dropdown = document.querySelector('.dropdown');
+    dropdown.classList.toggle('active');
+}
+//if clicked of close.
+document.addEventListener('click', event => {
+    let dropdown = document.querySelector('.dropdown');
+    if (!dropdown.contains(event.target)) {
+        dropdown.classList.remove('active');
+    }
+});
+}
+
